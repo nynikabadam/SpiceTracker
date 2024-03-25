b06e979b2927a0e1586c053b0bebcc5b6109bbc5
Sanjana Kumar <spkumar4@illinois.edu>
2024-03-24 21:20:14 -0500

    Initial Commit

---
+++ SpiceTracker2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpiceTracker2
+//
+//  Created by Sanjana Kumar on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
